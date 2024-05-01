8775fce803e9455842d9073b62b1356f90bbf9d6
Nkeh <bonyankeh@gmail.com>
2024-05-01 05:37:06 +0100

    created the task model

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    """Model definition for Todo."""
+
+    # TODO: Define fields here
+    title = models.CharField(max_length=200, null=False, blank= False)
+    description = models.TextField()
+    creation_time = models.DateTimeField(auto_now_add=True)
+    completion_time = models.DateTimeField()
+    
+
+    class Meta:
+        pass
+
+    def __str__(self):
+        return self.title
