7b9e4fef6481432e859fc236b543c3509b21949d
Nkeh <bonyankeh@gmail.com>
2024-05-04 05:30:04 +0100

    Worked on the templates and the views

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.taskview),
+    path('tasks/', views.taskList),
+    path('tasks/<int:pk>/', views.taskDetail),
     
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from .models import Task
 # Create your views here.
 
-def taskview(request):
+def taskList(request):
+    tasks = Task.objects.all()
     
-    return HttpResponse("Welcome")
+    return render(request, 'todoapp/task-list.html', {'tasks': tasks})
     
+
+def taskDetail(request, pk):
+    task = Task.objects.get(pk=pk)
+    return render(request, 'todoapp/task-detail.html', {'task': task})
